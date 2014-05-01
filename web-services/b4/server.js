b201c6d2cbb327e788069fa246774d388188966c
Wes Williams <wes.williamz@gmail.com>
2014-05-01 20:16:13 +0800

    server and services for better book bundler builder (b4)

---
+++ web-services/b4/server.js
@@ -0,0 +1,19 @@
+'use strict';
+const
+  express = require('express'),
+  logger = require('morgan'),
+  app = express();
+
+app.use(logger('dev'));
+
+const config = {
+  bookdb: 'http://localhost:5984/books/',
+  b3db: 'http://localhost:5984/b4/'
+};
+require('./lib/book-search.js')(config, app);
+require('./lib/field-search.js')(config, app);
+require('./lib/bundle.js')(config, app);
+
+app.listen(3000, function(){
+  console.log("ready captain.");
+});
