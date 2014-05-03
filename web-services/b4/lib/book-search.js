af464f82623c4c160abef08f87be5c6b7fbdffec
Wes Williams <wes.williamz@gmail.com>
2014-05-03 16:47:18 +0800

    async by_:view

--- web-services/b4/lib/book-search.js
+++ web-services/b4/lib/book-search.js
@@ -19,12 +19,12 @@ const
 
 function asyncRequest(request, res) {
   let qRequest = Q.denodeify(request);
-  return Q.async(function*(path) {
-    console.log('path: ' + path);
+  return Q.async(function*() {
+    console.log('args: ' + arguments);
     let
-      args = yield qRequest(path),
-        couchRes = args[0],
-        body = args[1];
+      values = yield qRequest.apply(null, arguments),
+        couchRes = values[0],
+        body = values[1];
     if (couchRes.statusCode !== 200) {
       console.log('couch err: ' + couchRes.statusCode);
       res.json(couchRes.statusCode, JSON.parse(body));
@@ -37,51 +37,43 @@ function asyncRequest(request, res) {
 
 module.exports = function (config, app) {
 
-  function findViews(callback, res) {
+  function findViews(res) {
     console.log('find views');
     let get = asyncRequest(request.get, res);
-    Q.async(function*() {
+    return Q.async(function*() {
       let views = yield get(config.bookdb + '_design/books/');
       console.log("views: " + JSON.stringify(views));
-      callback(null, views.views);
-    })()
-      .catch(function (err) {
-        console.log('error finding available views: ' + err);
-        callback(502, { error: "bad_gateway", reason: err.code });
-      });
+      return yield views.views;
+    });
   }
 
-  function filterViews(filterBy, callback) {
-    return function (err, views) {
-      if (err) {
-        console.log(err);
-        callback(err);
-        return;
-      }
-      let filteredViews = underscore.chain(views)
-        .keys(function (name) {
-          return name.indexOf(filterBy) === 0;
-        }).value();
-      console.log('filtered views: ' + filteredViews);
-      callback(null, filteredViews);
-    };
+  function filterViews(views, filterBy) {
+    let filteredViews = underscore.chain(views)
+      .keys(function (name) {
+        return name.indexOf(filterBy) === 0;
+      }).value();
+    console.log('filtered views: ' + filteredViews);
+    return filteredViews;
   }
 
   function viewNotAvailable(availableViewNames, viewName) {
-    return !underscore.contains(availableViewNames, viewName);
+    if (!underscore.contains(availableViewNames, viewName)) {
+      throw {
+        code: 400,
+        error: 'view not available',
+        reason: ''
+      };
+    }
   }
 
   app.get('/api/search/book/by_:view', function (req, res) {
-    findViews(filterViews('by_', function (err, availableViews) {
-      if (err) {
-        res.json(502, { error: "bad_gateway", reason: err.code });
-        return;
-      }
-      if (viewNotAvailable(availableViews, 'by_' + req.params.view)) {
-        res.json(400, { error: 'view not available' });
-        return;
-      }
-      request({
+    let qRequest = Q.denodeify(request);
+    Q.async(function*() {
+      let views, filteredViews, values, couchRes, body, books = {};
+      views = yield findViews(res)();
+      filteredViews = filterViews(views, 'by_');
+      viewNotAvailable(filteredViews, 'by_' + req.params.view);
+      values = yield qRequest({
         method: 'GET',
         url: config.bookdb + '_design/books/_view/by_' + req.params.view,
         qs: {
@@ -89,29 +81,22 @@ module.exports = function (config, app) {
           reduce: false,
           include_docs: true
         }
-      }, function (err, couchRes, body) {
-
-        // couldn't connect to CouchDB
-        if (err) {
-          res.json(502, { error: "bad_gateway", reason: err.code });
-          return;
-        }
-
-        // CouchDB couldn't process our request
-        if (couchRes.statusCode !== 200) {
-          res.json(couchRes.statusCode, JSON.parse(body));
-          return;
-        }
-
-        // send back simplified documents we got from CouchDB
-        let books = {};
-        JSON.parse(body).rows.forEach(function (elem) {
-          books[elem.doc._id] = elem.doc.title;
-        });
-        res.json(books);
-
       });
+      couchRes = values[0];
+      body = values[1];
+      if (couchRes.statusCode !== 200) {
+        res.json(couchRes.statusCode, JSON.parse(body));
+        return;
+      }
 
-    }), res);
+      JSON.parse(body).rows.forEach(function (elem) {
+        books[elem.doc._id] = elem.doc.title;
+      });
+      res.json(books);
+    })()
+      .catch(function (err) {
+        console.log('error finding available books: ' + JSON.stringify(err));
+        res.json(502, { error: "bad_gateway", reason: err.code });
+      });
   });
 };
